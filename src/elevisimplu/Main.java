b9db3ed50fca0f82765fc8ba3d9bef8fdbfe68b6
Adrian Mascan <adrianmascan@yahoo.com>
2016-10-03 22:08:52 +0300

    final

--- src/elevisimplu/Main.java
+++ src/elevisimplu/Main.java
@@ -5,18 +5,22 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 public class Main {
     public static void main(String[] args) {
-
-    List<Elev> elevi = new ArrayList<>();
+        Map<String, Double> map = new HashMap<>();
+        List<Elev> elevi = new ArrayList<>();
         for(String element: FileRead.read("elevi.csv")) {
             Elev a = new Elev(element);
             elevi.add(a);
+            map.put(a.nume, a.materii);
             System.out.println(a);
         }
+
+
+        for (String key : map.keySet()) {
+            System.out.println(key + " " + map.get(key));
+        }
     }
-
-    Map m1 = new HashMap();
-
 
 }
